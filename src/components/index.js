6d9d9bcd598589d5f4c3b78c1cda369cac6b64f8
Ferigit <faramarz.razmi@gmail.com>
2023-01-13 02:08:28 +0330

    update common components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as Form } from "./Form";
 export { default as Container } from "./Container";
+export { default as TD } from "./TD";
+export { default as TR } from "./TR";
