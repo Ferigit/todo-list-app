32601de8b87f0ed0508dd98ab497df7c8332c6f9
Ferigit <faramarz.razmi@gmail.com>
2023-01-12 15:07:37 +0330

    add common components

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 10px 1em;
+  border-radius: 6px;
+  background: rgb(118,120,224);
+  color: #fff;
+`;
+
+export default Button;
